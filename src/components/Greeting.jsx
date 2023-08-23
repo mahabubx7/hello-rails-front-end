1b0791f631872def9d105745f9af171124973a2d
Mahabub Alam <mahabubx7@gmail.com>
2023-08-23 14:57:25 +0600

    setup(redux): :hammer: added-greeting-component

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,7 +1,23 @@
-function Greeting() {
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchGreeting } from '../redux/greeting/greetingSlice';
+
+export default function Greeting() {
+  const dispatch = useDispatch();
+  const { greeting, loading } = useSelector((store) => store.greeting);
+
+  useEffect(() => {
+    dispatch(fetchGreeting());
+  }, [dispatch]);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
   return (
-    <h2>Greetings</h2>
+    <div>
+      <h1>Greetings</h1>
+      <h2>{greeting.message}</h2>
+    </div>
   );
 }
-
-export default Greeting;
